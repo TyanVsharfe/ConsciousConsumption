4ecbf07d467b032c34d29b591bb61616213c9b92
Tyan v sharfe <thisonlystudy@gmail.com>
2023-05-11 13:12:45 +0500

    Added display of labels

--- src/main/java/com/spring/consciousconsumption/controller/CoreController.java
+++ src/main/java/com/spring/consciousconsumption/controller/CoreController.java
@@ -1,16 +1,20 @@
 package com.spring.consciousconsumption.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Controller
 public class CoreController {
-
     @GetMapping("/")
     public String home(Model model) throws IOException {
         String keyAPI = Files.readString(Paths.get("key.txt"));
